f0ed8cdef96f2ab9d4bdd1346e5d4a6ea8a5d762
JeffreyFowels <37136141+JeffreyFowels@users.noreply.github.com>
2021-11-24 16:35:37 +0200

    Update AVDDesktopuser.ps1

--- AVDDesktopuser.ps1
+++ AVDDesktopuser.ps1
@@ -4,10 +4,11 @@ $AzCredentialsAsset = 'AzureCredentials'
 $AzCredentials = Get-AutomationPSCredential -Name $AzCredentialsAsset
 $AzCredentials.password.MakeReadOnly()
 Connect-AzAccount -Credential $AzCredentials
-$usernames2 = Get-AutomationVariable -Name 'usernames' 
+$usernames2 = Get-AutomationVariable -Name 'AVDUserAccess' 
 $usernames3 = $usernames2 -split (',')
+$ResourceName = Get-AutomationVariable -Name 'Resource' 
 ##^(?:(?<username>[^@]+)@(?<domain>.+)|(?<domain>[^\\]+)\\(?<username>.+))$
 
 
 foreach ($upn in $usernames3) {
-New-AzRoleAssignment -SignInName $upn -RoleDefinitionName "Desktop Virtualization User" -ResourceName cbrazzaavd-flim-Desktops -ResourceGroupName RSG-WVD -ResourceType 'Microsoft.DesktopVirtualization/applicationGroups' }
+New-AzRoleAssignment -SignInName $upn -RoleDefinitionName "Desktop Virtualization User" -ResourceName $ResourceName -ResourceGroupName RSG-WVD -ResourceType 'Microsoft.DesktopVirtualization/applicationGroups' }
